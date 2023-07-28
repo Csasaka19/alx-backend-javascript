2890309654e70cdda6600500602e580eb5092ec9
Clive Sasaka <clivesasaka@gmail.com>
2023-07-28 15:49:34 +0300

    Working with a static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing extends Currency{
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+  get amount() {
+    return this._amount;
+  } set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+        throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  } get currency() {
+      return this._currency;
+  } set currency(newCurrency) {
+      if (!(newCurrency instanceof Currency)) {
+        throw new TypeError('Currency must be a Currency');
+      }
+      this._currency = newCurrency;
+  } displayFullPrice() {
+      return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  } static convertPrice(amount, ConversionRate) {
+      if (typeof amount !== 'number') {
+        throw new TypeError('Amount must be a number');
+      } if (typeof ConversionRate !== 'number') {
+        throw new TypeError('ConversionRate must be a number');
+      } 
+      return amount * ConversionRate;
+    }
+}
