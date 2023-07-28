a561a99e09e5181be068c50013456896102fdda0
Clive Sasaka <clivesasaka@gmail.com>
2023-07-28 16:29:13 +0300

    Working with symbol.species reflector

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,27 @@
+export default class Car {
+    constructor(brand, motor, color) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+    get brand() {
+        return this._brand;
+    } set brand(newBrand) {
+        this._brand = newBrand;
+    }
+    get motor() {
+        return this._motor;
+    } set motor(newMotor) {
+        this._motor = newMotor;
+    }
+    get color() {
+        return this._color;
+    } set color(newColor) {
+        this._color = newColor;
+    }
+    cloneCar() {
+        const Species = this.constructor[Symbol.species];
+
+        return new Species();
+    }
+    }
