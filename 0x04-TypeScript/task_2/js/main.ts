d0bf3ebd026679c99eec9ce3323fbb0c16ac0967
Clive Sasaka <clivesasaka@gmail.com>
2023-08-03 14:52:53 +0300

    Work execution and work status verified

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -42,3 +42,14 @@ export function createEmployee(salary: number | string): Director | Teacher{
     if (typeof salary === "number" && salary < 500) return new Teacher();
     return new Director();
 }
+
+export function isDirector(employee: Director | Teacher): employee is Director{
+    return (employee instanceof Director);
+}
+
+export function executeWork(employee: (Director | Teacher)) {
+    if (isDirector(employee)) {
+      return (employee as Director).workDirectorTasks();
+    }
+    return (employee as Teacher).workTeacherTasks();
+  }
