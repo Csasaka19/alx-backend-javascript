cc369ff95e9620949ee714d3d80a9896ad71e677
Clive Sasaka <clivesasaka@gmail.com>
2023-09-22 20:59:56 +0300

    Going through a database sychronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,9 +1,31 @@
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        process.stdout.write(data);
+const countStudents = (path) => {
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+        const lines = data.split('\n');
+        let count = 0;
+        const fields = {};
+        for (const line of lines) {
+        if (line !== '') {
+            count += 1;
+            const student = line.split(',');
+            if (!fields[student[3]]) {
+            fields[student[3]] = [];
+            }
+            fields[student[3]].push(student[0]);
+        }
+        }
+        console.log(`Number of students: ${count}`);
+        for (const field in fields) {
+        if (field) {
+            const list = fields[field];
+            console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+        }
+        }
+    } catch (error) {
+        throw new Error('Cannot load the database');
     }
-    });
+    };
+
+module.exports = countStudents;
